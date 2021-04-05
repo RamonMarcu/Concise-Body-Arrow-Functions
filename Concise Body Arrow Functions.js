2bc13b684e912cb2f34b79e4bf6c6961534f4bb4
Ramon Marcu <ramondotmarcu@gmail.com>
2021-04-05 20:03:44 +0300

    Add files via upload

---
+++ Concise Body Arrow Functions.js
@@ -0,0 +1,13 @@
+//main.js
+
+
+const plantNeedsWater = (day) => {
+  return day === 'Wednesday' ? true : false;
+};
+
+
+// refactored plantNeedsWater() to be a concise body:
+
+const plantNeedsWater = day => day === 'Wednesday' ? true : false;
+
+
